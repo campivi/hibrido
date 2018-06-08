6268e284a8bdcac2cc66c0072961a4228180ba50
campivi <campivi13@gmail.com>
2018-06-08 01:30:48 -0500

    avances 2.0

--- BlankCordovaApp1/BlankCordovaApp1/www/scripts/main.js
+++ BlankCordovaApp1/BlankCordovaApp1/www/scripts/main.js
@@ -5,3 +5,36 @@
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
+
+//PERSONALIZE
+function AddMaterialsToList(){
+    var ul = document.getElementById("dynamic-list");
+    var candidate = document.getElementById("material_new");
+    var li = document.createElement("li");
+    li.setAttribute('id', candidate.value);
+    li.appendChild(document.createTextNode(candidate.value));
+    ul.appendChild(li);
+}
+
+function LessMaterialsToList() {
+    var ul = document.getElementById("dynamic-list");
+    var candidate = document.getElementById("material_new");
+    var item = document.getElementById(candidate.value);
+    ul.removeChild(item);
+}
+
+function AddToDoList() {
+    var ul = document.getElementById("dynamic-list");
+    var candidate = document.getElementById("todo_new");
+    var li = document.createElement("li");
+    li.setAttribute('id', candidate.value);
+    li.appendChild(document.createTextNode(candidate.value));
+    ul.appendChild(li);
+}
+
+function LessToDoList() {
+    var ul = document.getElementById("dynamic-list");
+    var candidate = document.getElementById("todo_new");
+    var item = document.getElementById(candidate.value);
+    ul.removeChild(item);
+}
